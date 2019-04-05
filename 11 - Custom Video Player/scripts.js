da2a60ac58a51d71eb72db02fe8d350cc64bd9b8
admin <admin@admins-MacBook-Air.local>
2019-04-05 16:51:18 +0700

    05/04/2019

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,28 +1,26 @@
 const player = document.querySelector('.player')
-const video = player.querySelector('.viewer')
-const progress = player.querySelector('.progress')
-const progressBar = player.querySelector('.progress__filled')
-const toggle = player.querySelector('.toggle')
-const skipButtons = player.querySelectorAll('[data-skip]')
-const ranges = player.querySelectorAll('input[type="range"]')
-// console.log(ranges)
-function togglePlay(){
-    (video.paused) ? video.play(): video.pause();
+const video = document.querySelector('.player__video')
+const progress = document.querySelector('.progress')
+const progressBar = document.querySelector('.progress__filled')
+const toggle = document.querySelector('.player__button')
+const skipButtons = document.querySelectorAll('[data-skip]')
+const ranges = document.querySelectorAll('.player__slider')
+
+function togglePlay() {
+    video.paused ? video.play(): video.pause()
 }
 function updateButton() {
-    const icon = this.paused ? '►' : '❚ ❚';
-    toggle.textContent = icon;
-    console.log('updated')
+    const icon = video.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon
 }
-function skip(){
-    // console.log(this.dataset.skip)
-    // console.log(video.currentTime)
-    video.currentTime += parseFloat(this.dataset.skip) 
+function skip() {
+    const timeSkip = this.dataset.skip
+    video.currentTime += parseFloat(timeSkip) 
+    console.dir(video)
 }
-function handleRangeUpdate() {
+function handleRangeUpdate(e) {
     video[this.name] = this.value
-    // console.log(this)
-    // console.log(this.value)
+    // console.log(this.name, this.value)
 }
 function handleProgress() {
     const percent = (video.currentTime / video.duration) * 100
@@ -30,25 +28,22 @@ function handleProgress() {
     
 }
 function scrub(e) {
-    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration
-    console.log(progress.dataset)
-    video.currentTime = scrubTime;
+    console.dir(progressBar)
+    per = e.offsetX / progress.offsetWidth
+    video.currentTime = per * video.duration
+    console.log(per, video.duration)
 }
-let mouseDown = false;
 video.addEventListener('click', togglePlay)
-video.addEventListener('play', updateButton)
-video.addEventListener('pause', updateButton)
-video.addEventListener('timeupdate', handleProgress)
-
-progress.addEventListener('click', scrub)
-progress.addEventListener('mousemove', (e) => mouseDown && scrub(e))
-progress.addEventListener('mousedown', () => mouseDown = true)
-progress.addEventListener('mouseup', () => mouseDown = false)
-
 toggle.addEventListener('click', togglePlay)
-skipButtons.forEach(button => button.addEventListener('click', skip))
-ranges.forEach(range => range.addEventListener('change', handleRangeUpdate))
-ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate))
-// full screen
-const fullSce = document.querySelector('.fullScreen')
-fullSce.addEventListener('click', () => player.requestFullscreen())
+video.addEventListener('play', updateButton)
+video.addEventListener('pause', updateButton)
+const btnFullscreen = document.querySelector('.fullScreen')
+btnFullscreen.addEventListener('click', () => video.requestFullscreen())
+skipButtons.forEach(btn => {
+    btn.addEventListener('click', skip)
+})
+ranges.forEach(range => {
+    range.addEventListener('change', handleRangeUpdate)
+})
+video.addEventListener('timeupdate', handleProgress)
+progress.addEventListener('click', scrub)
